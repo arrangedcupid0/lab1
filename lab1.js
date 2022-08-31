fca120aea81487faf067408dce9306cca0543bfd
arrangedcupid0 <mstoll3@uwyo.edu>
2022-08-31 16:49:43 -0600

    fixed lol

--- lab1.js
+++ lab1.js
@@ -51,13 +51,13 @@ function insertionSort(arr) {
 
 function insertionSortReverse(arr)
 {
-	for(var i = (arr.length - 1); i > 0; i--)
+	for(var i = (arr.length - 1); i >= 0; i--)
 	{
 		var val = arr[i];
 		var j;
-		for(j = i; j < arr.length && arr[j] < val; j++)
+		for(j = i; j < arr.length && arr[j+1] < val; j++)
 		{
-			arr[j+1] = arr[j]; 
+			arr[j] = arr[j+1]; 
 		}
 		arr[j] = val;
 	}
